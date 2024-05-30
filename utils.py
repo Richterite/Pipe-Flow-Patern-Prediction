2782d0cf3039d7ded94e2ce4f493d17074a33888
NormalPeeple <fikrifauzy156@gmail.com>
2024-05-30 16:37:22 +0700

    feat: Add Floating Point Operation Counter

--- utils.py
+++ utils.py
@@ -393,3 +393,61 @@ def f1_score(y_true, y_pred):
     precision_result = precision(y_true, y_pred)
     recall_result = recall(y_true, y_pred)
     return 2*((precision_result *recall_result)/ (precision_result +recall_result+K.epsilon()))
+
+
+def get_flops(model, model_inputs) -> float:
+        """
+        Menghitung Banyaknya Floation Point Operation pada keras model. Perhitungan menggunakan
+        legacy version dari tensorflow v1
+
+        @Parameter
+        *model : Keras.Model
+        *model_inputs : tf.Constant
+
+        """
+        # if not hasattr(model, "model"):
+        #     raise wandb.Error("self.model must be set before using this method.")
+
+        if not isinstance(
+            model, (tf.keras.models.Sequential, tf.keras.models.Model)
+        ):
+            raise ValueError(
+                "Calculating FLOPS is only supported for "
+                "`tf.keras.Model` and `tf.keras.Sequential` instances."
+            )
+
+        from tensorflow.python.framework.convert_to_constants import (
+            convert_variables_to_constants_v2_as_graph,
+        )
+
+        # Compute FLOPs for one sample
+        batch_size = 1
+        inputs = [
+            tf.TensorSpec([batch_size] + inp.shape[1:], inp.dtype)
+            for inp in model_inputs
+        ]
+
+        # convert tf.keras model into frozen graph to count FLOPs about operations used at inference
+        real_model = tf.function(model).get_concrete_function(inputs)
+        frozen_func, _ = convert_variables_to_constants_v2_as_graph(real_model)
+
+        # Calculate FLOPs with tf.profiler
+        run_meta = tf.compat.v1.RunMetadata()
+        opts = (
+            tf.compat.v1.profiler.ProfileOptionBuilder(
+                tf.compat.v1.profiler.ProfileOptionBuilder().float_operation()
+            )
+            .with_empty_output()
+            .build()
+        )
+
+        flops = tf.compat.v1.profiler.profile(
+            graph=frozen_func.graph, run_meta=run_meta, cmd="scope", options=opts
+        )
+
+        tf.compat.v1.reset_default_graph()
+
+        # convert to GFLOPs
+        return (flops.total_float_ops)
+    
+    
