370bd771a1d6baefaef5fa27700adab038a5a6e0
NormalPeeple <fikrifauzy156@gmail.com>
2025-03-16 06:01:09 +0700

    ✨ feat(config): add new data config file

---
+++ src/config/data.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Const:
+    N_POINTS_Y : int = 64
+    ASPECT_RATIO : int = 4
+    KINEMATIC_VISCOSITY : float = 0.001
+    TIME_STEP_LENGTH : float = 0.001
+    N_TIME_STEPS : int = 6000
+    PLOT_EVERY : int = 375
+    STEP_HEIGHT_POINTS : int = 18
+    STEP_WIDTH_POINTS : int = 40
+    N_PRESSURE_POISSON_ITERATIONS : int = 50
+    N_POINTS_X = (N_POINTS_Y ) * ASPECT_RATIO
+    CELL_LENGTH = 1.0 / (N_POINTS_Y - 1)
+
+
+@dataclass
+class DataShape:
+    TARGET_LENGTH : int = 16
+    BATCH_SIZE: int = 36
+    TIME_STEP : int = 16
+    ROW : int = 64 
+    COL : int = 256 
+    CHANNEL : int = 2
+    OBSTACLE_HEIGHT_VARIETY = [num for num in range(8, 44)]
